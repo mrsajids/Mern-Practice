f311303a4af9106647aac5c89a341e4aebe2ee53
Sajeed <syedsajeed6969@gmail.com>
2024-06-05 00:26:08 +0530

    update

--- src/sorting/SortTable.js
+++ src/sorting/SortTable.js
@@ -1,21 +1,33 @@
 import Table from 'react-bootstrap/Table';
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 
 function SmallExample() {
+    const [data,setData]=useState([])
     let array = [
         { id: 1, fn: "Mark", ln: "Henry", un: 'dfjkdff' },
         { id: 2, fn: "Vark", ln: "Henry", un: 'dfjkdff' },
         { id: 3, fn: "Amit", ln: "Henry", un: 'dfjkdff' },
         { id: 4, fn: "Berry", ln: "Henry", un: 'dfjkdff' },
-        { id: 5, fn: "John", ln: "Henry", un: 'dfjkdff' },
-
+        { id: 5, fn: "amti", ln: "Henry", un: 'dfjkdff' },
     ]
     let str = ['dad', 'rad', 'cad', 'merry', 'jerry']
     let no = [19, 34, 5,25, 23, 25]
     function onSort() {
         //sorting an array
-        console.log(no.sort((a, b) => b - a));
+        // console.log([...no].sort((a, b) => b - a));
+        // console.log(no);
+
+        // sorting array strings
+        setData([...array].sort((a, b) => {
+            // for ascending order 
+            if(a.fn.toLowerCase()>b.fn.toLowerCase()) return 1;
+            if(b.fn.toLowerCase()>a.fn.toLowerCase()) return -1;
+            return 0;
+        }));
+
+        // sorting array non english strings use localcompare
+        
     }
     return (
         <Container className='mt-5'>
@@ -30,7 +42,7 @@ function SmallExample() {
                 </thead>
                 <tbody>
                     {
-                        array.map((item, i) => (
+                        (data||array).map((item, i) => (
                             <tr>
                                 <td>{item.id}</td>
                                 <td>{item.fn}</td>
